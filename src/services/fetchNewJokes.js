f6cc8c3dc292dde7bba25cd1a1d0f848bbe0e931
Lynn Kim <lynn.kim@brandung.de>
2023-07-10 11:30:07 +0200

    remove for loop

--- src/services/fetchNewJokes.js
+++ src/services/fetchNewJokes.js
@@ -7,28 +7,28 @@ export default function fetchNewJokes(onUpdateJokes, onUpdateLoaded) {
   const fetches = [];
   const data = [];
 
-  for (let i = 0; i < 10; i++) {
-    let request = axios.get("https://icanhazdadjoke.com/", {
-      headers: { Accept: "application/json" },
-    });
-
-    request.then((response) => {
-      const { id, joke } = response.data;
-      const isDuplicate = data.some((j) => j.id === id);
-
-      if (!isDuplicate) {
-        data.push({
-          id,
-          joke,
-        });
-      }
-    });
-
-    fetches.push(request);
-  }
-
-  Promise.all(fetches).then(() => {
-    onUpdateLoaded(true);
-    onUpdateJokes(data);
+  // for (let i = 0; i < 10; i++) {
+  let request = axios.get("https://icanhazdadjoke.com/", {
+    headers: { Accept: "application/json" },
   });
+
+  request.then((response) => {
+    const { id, joke } = response.data;
+    const isDuplicate = data.some((j) => j.id === id);
+
+    if (!isDuplicate) {
+      data.push({
+        id,
+        joke,
+      });
+    }
+  });
+
+  fetches.push(request);
 }
+
+Promise.all(fetches).then(() => {
+  onUpdateLoaded(true);
+  onUpdateJokes(data);
+});
+// }
